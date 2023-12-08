faddff999a29ff6555b2fcb4ebcfb967eb6d6ba2
hamzaderbaz <hamza.derbaz@gmail.com>
2023-12-08 16:52:49 +0200

    update project

--- Vezeeta/Vezeeta/Program.cs
+++ Vezeeta/Vezeeta/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Vezeeta.Infrastructure; 
+using Vezeeta.Core; 
 
 namespace Vezeeta
 {
-    public class Program
+    public class NewProgram
     {
         public static void Main(string[] args)
         {
@@ -14,7 +19,58 @@ namespace Vezeeta
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>(); // Point to Startup class
+                    webBuilder.UseStartup<NewStartup>(); 
                 });
+    }
+
+    public class NewStartup
+    {
+        public NewStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCoreServices(Configuration);
+            services.AddInfrastructureServices(Configuration);
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            // Enable Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vezeeta API V1");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+        }
     }
 }
